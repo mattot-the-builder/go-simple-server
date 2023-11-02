dae17117cf8b6e1d3392541ee8031303ee4561a1
mattot-the-builder <135392742+mattot-the-builder@users.noreply.github.com>
2023-11-03 04:26:27 +0800

    Added routing

--- main.go
+++ main.go
@@ -2,31 +2,25 @@ package main
 
 import (
 	"fmt"
-	"log"
 	"net/http"
-	"strings"
 )
 
-func sayHelloName(w http.ResponseWriter, r *http.Request) {
-	r.ParseForm()
-	fmt.Println(r.Form)
-	fmt.Println("path", r.URL.Path)
-	fmt.Println("scheme", r.URL.Scheme)
-	fmt.Println(r.Form["url_long"])
-	for k, v := range r.Form {
-		fmt.Println("key:", k)
-		fmt.Println("val:", strings.Join(v, ""))
-	}
-	fmt.Fprintf(w, "Hello mattot!")
+type MyMux struct{}
 
+func (p *MyMux) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	if r.URL.Path == "/" {
+		sayhelloName(w, r)
+		return
+	}
+	http.NotFound(w, r)
+	return
+}
+
+func sayhelloName(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Hello myroute!")
 }
 
 func main() {
-	http.HandleFunc("/", sayHelloName)
-	fmt.Println("Starting server at http://localhost:8000")
-	err := http.ListenAndServe(":8000", nil)
-
-	if err != nil {
-		log.Fatal("ListenAndServe: ", err)
-	}
+	mux := &MyMux{}
+	http.ListenAndServe(":9090", mux)
 }
